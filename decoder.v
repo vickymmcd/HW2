fd59a25b84e335faa4c08bcd0174f56a9c9f4247
vickymmcd <vickymmcd@gmail.com>
2018-09-14 22:08:20 -0400

    starting hw, finished decoder

--- decoder.v
+++ decoder.v
@@ -1,8 +1,15 @@
+// define gates with delays
+`define AND and #50
+`define OR or #50
+`define NOT not #50
+
 // Decoder circuit
 
 module behavioralDecoder
 (
     output out0, out1, out2, out3,
+    output enabledA0, enabledA1,
+    output notA0, notA1, notA0enabled, notA1enabled,
     input address0, address1,
     input enable
 );
@@ -18,5 +25,16 @@ module structuralDecoder
     input enable
 );
     // Your decoder code here
-endmodule
+    wire enabledA0, enabledA1, notA0, notA1, notA0enabled, notA1enabled;
+    `AND and1(enabledA0, address0, enable);
+    `AND and2(enabledA1, address1, enable);
+    `NOT not1(notA0, address0);
+    `NOT not2(notA1, address1);
+    `AND and3(notA0enabled, notA0, enable);
+    `AND and4(notA1enabled, notA1, enable);
+    `AND and5(out0, notA0enabled, notA1enabled);
+    `AND and6(out1, enabledA0, notA1enabled);
+    `AND and7(out2, notA0enabled, enabledA1);
+    `AND and8(out3, enabledA0, enabledA1);
 
+endmodule
